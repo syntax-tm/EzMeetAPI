aa73862d0b728eaadad3d8ef29ea0dd7a4795873
syntax-tm <AtV3Tjmi4YmpJFX9aSsFAv6GWMsz2yyPua4aJPhTE3pR>
2022-09-24 13:30:25 -0500

    Added Google calendar API package and extensions

---
+++ Common/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace EzMeetAPI;
+
+public static class Extensions
+{
+
+    public static bool EqualsIgnoreCase(this string source, string target)
+    {
+        if (source is null)
+        {
+            return target is null;
+        }
+
+        return source.Equals(target, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static string AsJson(this object source, bool indented = true)
+    {
+        var formatting = indented ? Formatting.Indented : Formatting.None;
+        return JsonConvert.SerializeObject(source, formatting);
+    }
+
+}
