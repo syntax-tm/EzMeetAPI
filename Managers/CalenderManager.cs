782ed37dc5c04240e5b9a492103f1718fcb7fcdd
Cameron Bourque <28263454+CameronBourque@users.noreply.github.com>
2022-09-24 15:15:08 -0500

    Set up base for Calendar Manager class

---
+++ Managers/CalenderManager.cs
@@ -0,0 +1,35 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Discovery;
+using Google.Apis.Services;
+using Microsoft.AspNetCore.Http.Connections;
+
+namespace EzMeetAPI.Managers
+{
+    public class CalenderManager
+    {
+        //Temp variables to replace with db
+        public static UserCredential token;
+        private string calID = "primary";
+        private string eventID = "1";
+
+        public void createEvent()
+        {
+            //TODO
+        }
+
+        public void suggestEvent()
+        {
+            //TODO
+        }
+
+        public Events getBookings()
+        {
+            //TODO
+            Calendar service = new Calendar();
+            Events events = new Events();
+
+            return null;
+        }
+    }
+}
