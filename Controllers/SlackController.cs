feab07218544473ba6bb7a1c25c5b9a05cedfe2b
syntax-tm <AtV3Tjmi4YmpJFX9aSsFAv6GWMsz2yyPua4aJPhTE3pR>
2022-09-24 14:01:49 -0500

    Added Google NuGet packages

---
+++ Controllers/SlackController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EzMeetAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SlackController : Controller
+{
+    [HttpGet]
+    public string GetScheduleMenu()
+    {
+        return null;
+    }
+
+    [HttpPost]
+    public void CalendarItem()
+    {
+
+    }
+}
